8e535357455ed0c61bd27386899f03df29439f2b
Akanksha Singh <akanksingh@enphaseenergy.com>
2022-11-18 22:28:27 +0530

    Setup start script

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     messageOne.textContent = 'Loading...';
 
-    fetch(`http://localhost:3000/weather?address=${location}`)
+    fetch(`/weather?address=${location}`)
     .then(response => response.json().then((data) => {
        if(data.error){
         messageOne.textContent = data.error;
